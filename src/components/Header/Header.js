472e3c8ab0545933cc4fb52005f236667fb02bd2
Bryan Patrick <54081465+arthvadrr@users.noreply.github.com>
2023-03-02 01:51:30 -0600

    created animated text printer

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,24 +1,40 @@
+import { useState, useEffect } from 'react';
 import typeText from '../../scripts/typeText';
 import './Header.scss';
 
 const Header = () => {
+  const [loaderArr, setLoaderArr] = useState('......'.split(''))
+  const [loader, setLoader] = useState('Loading.')
+  const [asciiArr, setAsciiArr] = useState(`   _____                   _             _      _        _   _           
+  |_   _|__ _ __ _ __ ___ (_)_ __   __ _| |    / \\   ___| |_(_)_   _____ 
+    | |/ _ \\ '__| '_ ' _ \\| | '_ \\ / _' | |   / _ \\ / __| __| \\ \\ / / _ \\
+    | |  __/ |  | | | | | | | | | | (_| | |  / ___ \\ (__| |_| |\\ V /  __/
+    |_|\\___|_|  |_| |_| |_|_|_| |_|\\__,_|_| /_/   \\_\\___|\\__|_| \\_/ \\___|
+ `.split(''))
+  const [welcomeArr, setWelcomeArr] = useState('Welcome to Terminal Landing (TM) Termlink'.split(''));
+  const [welcome, setWelcome] = useState('');
+  const [ascii1, setAscii1] = useState('')
+
+  useEffect(() => {
+    typeText(loader, setLoader, loaderArr, setLoaderArr, 300)
+    
+    if (loaderArr.length === 0) {
+      setLoader('')
+      typeText(welcome, setWelcome, welcomeArr, setWelcomeArr, 5)
+    }
+
+    if (welcomeArr.length === 0) {
+      typeText(ascii1, setAscii1, asciiArr, setAsciiArr, 0.5)
+    }
+  })
+
   return (
     <header className="App-header">
-      <div className="crt-on">{typeText('Welcome to', 200)}<span className="uppercase-text">Terminal Landing</span> (TM) Termlink</div>
+      <div>{loader}</div>
+      <div className="crt-on">{welcome}</div>
       <div className="ascii-art">
-      <pre className="crt-on">
-&nbsp;_____                   _             _      _        _   _           <br/>
-|_   _|__ _ __ _ __ ___ (_)_ __   __ _| |    / \   ___| |_(_)_   _____ <br/>
-&nbsp;&nbsp;| |/ _ \ '__| '_ ` _ \| | '_ \ / _` | |   / _ \ / __| __| \ \ / / _ \<br/>
-&nbsp;&nbsp;| |  __/ |  | | | | | | | | | | (_| | |  / ___ \ (__| |_| |\ V /  __/<br/>
-&nbsp;&nbsp;|_|\___|_|  |_| |_| |_|_|_| |_|\__,_|_| /_/   \_\___|\__|_| \_/ \___|<br/>
-                                                                        
-
-
-                                                            
-
-</pre>
-</div>
+        <pre className="crt-on">{ascii1}</pre>
+      </div>
     </header>
   );
 }
--- src/scripts/typeText.js
+++ src/scripts/typeText.js
@@ -1,5 +1,14 @@
-const typeText = (text, speed) => {
-    
+const sleep = time => new Promise(res => setTimeout(res, time));
+
+async function typeText(state, setState, arr, setArr, speed) {
+    await sleep(speed); // MUST come first (before re-render)
+
+    const shallowArr = [...arr];
+
+    if (shallowArr.length > 0) {
+        setState(state + shallowArr.shift());
+        setArr(shallowArr);
+    }
 }
 
 export default typeText;
