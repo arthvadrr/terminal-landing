c435b3a332cac6b4aef873ce71089a669b35fb60
Bryan Patrick <54081465+arthvadrr@users.noreply.github.com>
2023-03-27 01:10:43 -0500

    working hackingGame board

--- src/routes/HackingGame/HackingGame.js
+++ src/routes/HackingGame/HackingGame.js
@@ -3,35 +3,128 @@ import Nav from '../../components/Nav/Nav';
 import terms from './terms.js'
 import './HackingGame.scss';
 
-const createMemoryAddr = () => {
-    const alphabet = 'abcdefghijklmnopqrstuvwxyz';
-
-    let result = ['0', 'x', Math.floor(Math.random() * 9), Math.floor(Math.random() * 9), alphabet[Math.floor(Math.random() * alphabet.length)], Math.floor(Math.random() * 9)];
-
-    return result.join('');
-}
-
-const createMemoryAddrs = amount => {
-    const result = [];
-
-    for (let i = 0; i < amount; i++) {
-        result.push(createMemoryAddr());
+const hackingGameInit = (rows, rowLength, minWords, minDuds) => {
+    let result = {
+        password: '',
+        rows: []
     }
 
-    return result;
+    let usedWords = [];
+    let usedDuds = [];
+
+    const createMemoryAddr = () => {
+        const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+
+        let result = ['0', 'x', Math.floor(Math.random() * 9), Math.floor(Math.random() * 9), alphabet[Math.floor(Math.random() * alphabet.length)], Math.floor(Math.random() * 9)];
+
+        return result.join('');
+    }
+
+    const getPassword = () => {
+        const index = Math.floor(Math.random() * usedWords.length);
+        return usedWords[index];
+    }
+
+    const createFiller = length => {
+        const result = [];
+
+        while (length) {
+            result.push(result.push(terms.fillers[Math.floor(Math.random() * terms.fillers.length)]));
+            length--;
+        }
+
+        return result.join('');
+    }
+
+    const shuffleArr = arr => {
+        let index = arr.length;
+        let random;
+
+        while (index > 0) {
+            random = Math.floor(Math.random() * index);
+            index--;
+            
+            [arr[index], arr[random]] = [arr[random], arr[index]];
+        }
+
+        return arr;
+    }
+
+    const createRow = length => {
+        const result = [];
+
+        const odds = {
+            word: 3,
+            dud: 5,
+        }
+
+        let word = '';
+        let dud  = '';
+
+        const hasWord = 1 === Math.floor(Math.random() * odds.word);
+        const hasDud  = hasWord ? false : 1 === Math.floor(Math.random() * odds.dud);
+
+        console.log(hasWord);
+
+        if (hasWord) {
+            word = terms.words[Math.floor(Math.random() * terms.words.length)];
+            length -= word.length;
+            usedWords.push(word);
+            result.push(<button>word</button>);
+        }
+
+        if (hasDud) {
+            dud = terms.duds[Math.floor(Math.random() * terms.duds.length)];
+            length -= dud.length;
+            usedDuds.push(dud);
+            result.push(<button>{dud}</button>);
+        }
+
+        while (length > 0) {
+            result.push(terms.fillers[Math.floor(Math.random() * terms.fillers.length)])
+            length--;
+        }
+
+        return [createMemoryAddr(), ...shuffleArr(result)];
+    }
+
+    let amount = rows;
+    const meetsRequirements = false;
+
+    console.log(createRow(5))
+
+    while (!meetsRequirements) {
+        const row = createRow(rowLength);
+        result.rows.push(row.join(''));
+        amount--;
+
+        if (amount === 0) {
+            if (
+                usedWords.length < minWords ||
+                usedDuds.length < minDuds
+            ) {
+                amount      = rows;
+                usedWords   = [];
+                usedDuds    = [];
+                result.rows = [];
+
+            } else {
+                result.password = getPassword();
+                return result;
+            }
+        }
+    }
 }
 
-const getPassword = () => {
-    const range = terms.words.length;
-    const index = Math.floor(Math.random() * range);
-
-    return terms.words[index];
-}
 
 const HackingGame = ({screen, setScreen}) => {
 
+    const gameBoard = hackingGameInit(5, 20, 3, 1);
+
+    console.log('here')
+    console.log(gameBoard);
+
     const [attemptsLeft, setAttemptsLeft] = useState([1, 1, 1, 1]);
-    const [memoryAddrCol, setMemoryAddrCol] = useState(createMemoryAddrs(20));
     const [wordsCol, setWordsCol] = useState([])
 
     return (
@@ -48,17 +141,13 @@ const HackingGame = ({screen, setScreen}) => {
                     })}</span>
 
                     <div className="hacking-game-cols">
-                        <div className="memory-addr">
-                            {memoryAddrCol.map(addr => <div>{addr}</div>)}
-                        </div>
-                        <div className="words">
-                            {wordsCol}
+                        <div className="rows">
+
                         </div>
                         <div className="likeness">
                             
                         </div>
                     </div>
-                    <button onClick={() => console.log(getPassword())}>clicky</button>
                 </div>
             </div>
         </div>
