78ddc05a44d3983d6dfce51ad4c63c603e77056d
Bryan Patrick <54081465+arthvadrr@users.noreply.github.com>
2023-03-29 00:49:43 -0500

    added 404 and other hackinggame stuffs

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useRef } from 'react';
+import { useState } from 'react';
 import Nav from './components/Nav/Nav'
 import Home from './routes/Home/Home';
 import Projects from './routes/Projects/Projects';
@@ -8,103 +8,102 @@ import terms from './assets/terms';
 import './App.scss';
 
 const App = () => {  
-const hackingGameInit = (
-  gameBoardSettings, 
-  decAttempt,
-  resetAttempts
-) => {
-  const { 
-    rows, 
-    rowLength, 
-    minWords, 
-    minDuds,
-    wordOdds,
-    dudOdds
-  } = gameBoardSettings;
+  const hackingGameInit = (
+    gameBoardSettings, 
+    resetAttempts
+  ) => {
+    const { 
+      rows, 
+      rowLength, 
+      minWords, 
+      minDuds,
+      wordOdds,
+      dudOdds
+    } = gameBoardSettings;
 
-  let result = {
-    password: '',
-    rows: []
-  }
-
-  let usedWords = [];
-  let usedDuds  = [];
-  let nbspKey   = 0;
-
-  const createMemoryAddr = () => {
-    const alphabet = 'abcdefghijklmnopqrstuvwxyz';
-
-    let result = ['0', 'x', Math.floor(Math.random() * 9), Math.floor(Math.random() * 9), alphabet[Math.floor(Math.random() * alphabet.length)], Math.floor(Math.random() * 9), <span key={`nbsp-${nbspKey}`}>&nbsp;&nbsp;</span>];
-
-    nbspKey++;
-
-    return result;
-  }
-
-  const getPassword = () => {
-    const index = Math.floor(Math.random() * usedWords.length);
-    return usedWords[index];
-  }
-
-  const shuffleArr = arr => {
-    let index = arr.length;
-    let random;
-
-    while (index > 0) {
-        random = Math.floor(Math.random() * index);
-        index--;
-        
-        [arr[index], arr[random]] = [arr[random], arr[index]];
+    let result = {
+      password: '',
+      rows: []
     }
 
-    return arr;
-  }
+    let usedWords = [];
+    let usedDuds  = [];
+    let nbspKey   = 0;
 
-  const createRow = length => {
-    const result = [];
+    const createMemoryAddr = () => {
+      const alphabet = 'abcdefghijklmnopqrstuvwxyz';
 
-    let word = '';
-    let dud  = '';
+      let result = ['0', 'x', Math.floor(Math.random() * 9), Math.floor(Math.random() * 9), alphabet[Math.floor(Math.random() * alphabet.length)], Math.floor(Math.random() * 9), <span key={`nbsp-${nbspKey}`}>&nbsp;&nbsp;</span>];
 
-    const hasWord = 1 === Math.floor(Math.random() * wordOdds);
-    const hasDud  = hasWord ? false : 1 === Math.floor(Math.random() * dudOdds);
+      nbspKey++;
 
-    if (hasWord) {
-        word = terms.words[Math.floor(Math.random() * terms.words.length)];
-        length -= word.length;
-        usedWords.push(word);
-        result.push(
-            <button 
-                className="hacking-word" 
-                key={`word-${usedWords.length}`}
-                onMouseOver={(e) => handleOnMouseOver(e)}
-                onMouseOut={() => setHoveredWord('')}
-                onClick={(e) => handleWordSelect(e.target.innerHTML)}
-            >{word}</button>
-        );
+      return result;
     }
 
-    if (hasDud) {
-        dud = terms.duds[Math.floor(Math.random() * terms.duds.length)];
-        length -= dud.length;
-        usedDuds.push(dud);
-        result.push(
-        <button 
-            className="hacking-dud" 
-            key={`dud-${usedDuds.length}`}
-            onMouseOver={(e) => handleOnMouseOver(e)}
-            onMouseOut={() => setHoveredWord('')}
-            onClick={() => resetAttempts()}
-        >{dud}</button>);
+    const getPassword = () => {
+      const index = Math.floor(Math.random() * usedWords.length);
+      return usedWords[index];
     }
 
-    while (length > 0) {
-        result.push(`${terms.fillers[Math.floor(Math.random() * terms.fillers.length)]}`);
-        length--;
+    const shuffleArr = arr => {
+      let index = arr.length;
+      let random;
+
+      while (index > 0) {
+          random = Math.floor(Math.random() * index);
+          index--;
+          
+          [arr[index], arr[random]] = [arr[random], arr[index]];
+      }
+
+      return arr;
     }
 
-    return [createMemoryAddr(), ...shuffleArr(result)];
-  }
+    const createRow = length => {
+      const result = [];
+
+      let word = '';
+      let dud  = '';
+
+      const hasWord = 1 === Math.floor(Math.random() * wordOdds);
+      const hasDud  = hasWord ? false : 1 === Math.floor(Math.random() * dudOdds);
+
+      if (hasWord) {
+          word = terms.words[Math.floor(Math.random() * terms.words.length)];
+          length -= word.length;
+          usedWords.push(word);
+          result.push(
+              <button 
+                  className="hacking-word" 
+                  key={`word-${usedWords.length}`}
+                  onMouseOver={(e) => handleOnMouseOver(e)}
+                  onMouseOut={() => setHoveredWord('')}
+                  onClick={(e) => handleWordSelect(e.target.innerHTML)}
+              >{word}</button>
+          );
+      }
+
+      if (hasDud) {
+          dud = terms.duds[Math.floor(Math.random() * terms.duds.length)];
+          length -= dud.length;
+          usedDuds.push(dud);
+          result.push(
+          <button 
+              className="hacking-dud" 
+              key={`dud-${usedDuds.length}`}
+              onMouseOver={(e) => handleOnMouseOver(e)}
+              onMouseOut={() => setHoveredWord('')}
+              onClick={() => resetAttempts()}
+          >{dud}</button>);
+      }
+
+      while (length > 0) {
+          result.push(`${terms.fillers[Math.floor(Math.random() * terms.fillers.length)]}`);
+          length--;
+      }
+
+      return [createMemoryAddr(), ...shuffleArr(result)];
+    }
 
   let amount = rows;
   const meetsRequirements = false;
@@ -133,7 +132,7 @@ const hackingGameInit = (
   }
 
   const gameBoardSettings = {
-    rows:          30,
+    rows:          20,
     rowLength:     35,
     minWords:      12,
     minDuds:       3,
@@ -144,15 +143,15 @@ const hackingGameInit = (
   const decAttempt = () => {
     const shadow = attemptsLeft;
     shadow.pop();
-    console.log(gameBoard.password)
     setAttemptsLeft(() => [...shadow]);
   }
 
-  const handleOnMouseOver = (e, word) => {
+  const handleOnMouseOver = (e) => {
     setHoveredWord(e.target.innerHTML);
   }
 
   let likenessKey = 0;
+
   const handleWordSelect = (word) => {
     if (word !== gameBoard.password) {
       decAttempt();
@@ -167,7 +166,14 @@ const hackingGameInit = (
       }
 
       const likenessShadow = likenessCol;
-      likenessShadow.push(<div key={`likeness-${likenessKey}`}>{`>`} Likeness {`${counter}`}</div>)
+
+      likenessShadow.push(
+        <div className="likeness-result" key={`likeness-${likenessKey}`}>
+          <div>&gt; {word}</div>
+          <div>&gt; Entry denied.</div>
+          <div>{`>`} Likeness = {`${counter}`}</div>
+        </div>
+      )
       setLikenessCol([...likenessShadow]);
       likenessKey++;
 
@@ -180,7 +186,7 @@ const hackingGameInit = (
   
   const [attemptsLeft, setAttemptsLeft] = useState([1, 1, 1, 1]);
   const [screen, setScreen] = useState('Home');
-  const [gameBoard, setGameBoard] = useState(hackingGameInit(gameBoardSettings, decAttempt, resetAttempts));
+  const [gameBoard] = useState(hackingGameInit(gameBoardSettings, decAttempt, resetAttempts));
   const [hoveredWord, setHoveredWord] = useState('');
   const [likenessCol, setLikenessCol] = useState([]);
 
@@ -197,7 +203,7 @@ const hackingGameInit = (
       }
       {screen === 'Projects' && <Projects screen={screen} setScreen={setScreen}/>}
       {screen === 'About' && <About screen={screen} setScreen={setScreen} />}
-      {screen === 'HackingGame' && <HackingGame 
+      {screen === 'HackingGame' && attemptsLeft.length > 0 && <HackingGame 
         screen={screen} 
         setScreen={setScreen}
         gameBoard={gameBoard}
@@ -205,6 +211,9 @@ const hackingGameInit = (
         hoveredWord={hoveredWord}
         likenessCol={likenessCol}
       />}
+      {attemptsLeft.length === 0 &&
+      <div>404</div>
+      }
     </div>
   );
 }
